12942ba30cba0dbb41573b30a7e548033c39eaab
pedroblima <pedrob@iesb.edu.br>
2024-05-14 13:28:08 -0300

    8

--- app/controllers/prod_ingredientes_controller.ts
+++ app/controllers/prod_ingredientes_controller.ts
@@ -1,9 +1,42 @@
 // import type { HttpContext } from '@adonisjs/core/http'
 
-import ProdIngrediente from "../models/prod_ingrediente.js";
+import ProdIngredientes from "#models/prod_ingrediente";
+import { HttpContext } from "@adonisjs/core/http";
 
 export default class ProdIngredientesController {
-    async index() {
-        return await ProdIngrediente.all()
+
+    // Paginação de ProdIngredientess
+    async index({ request }: HttpContext) {
+        const page = request.input('page', 1)
+        const perPage = request.input('perPage', 10)
+        return await ProdIngredientes.query().paginate(page, perPage)
+    }
+
+    // Requisição por id passado por rota(parãmetros)
+    async show({ params }: HttpContext) {
+        return await ProdIngredientes.findOrFail(params.id)
+    }
+
+    // Método para criar algum ProdIngredientes pelo Json
+    async store({ request }: HttpContext) {
+        const dados = request.only(['produtoId', 'comandaId', 'quantidade'])
+        return await ProdIngredientes.create(dados)
+    }
+
+    async update({params, request}: HttpContext){
+        const produtosComanda = await ProdIngredientes.findOrFail(params.id)
+        const dados = request.only(['produtoId', 'comandaId', 'quantidade'])
+        
+        produtosComanda.merge(dados)
+        return await produtosComanda.save()
+    }
+
+    // Deletando pruduto pelo Id do banco de dados
+    async destroy({ params }: HttpContext) {
+        const produtosComanda = await ProdIngredientes.findOrFail(params.id)
+
+        await produtosComanda.delete()
+        return { msg: 'Registro deletado com sucesso', produtosComanda }
+
     }
 }
